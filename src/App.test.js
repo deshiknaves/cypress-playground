4fafe242ad4a8f4ab97a81ce7110a83c7990f96d
Deshi Rahim <nawal.rahim@gmail.com>
2020-09-06 21:48:00 +1000

    ✅ Update the test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import App from './App'
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  render(<App />)
+  expect(screen.getByRole('button', { name: /first/i })).toBeInTheDocument()
+  expect(screen.getByRole('button', { name: /second/i })).toBeInTheDocument()
+  expect(screen.getByRole('button', { name: /third/i })).toBeInTheDocument()
 })
