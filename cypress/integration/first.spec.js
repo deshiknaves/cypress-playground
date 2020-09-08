0dcad79d83548d67ec5b9479323eef03bba06ebf
Deshi Rahim <nawal.rahim@gmail.com>
2020-09-08 12:08:14 +1000

    🔥 Remove duplicate test

--- cypress/integration/first.spec.js
+++ cypress/integration/first.spec.js
@@ -4,15 +4,6 @@ describe('First', () => {
     cy.window().then(win => {
       console.log(win.msw)
     })
-    cy.findByRole('button', { name: /first/i }).click()
-    cy.findByRole('heading', { name: /first/i }).should('be.visible')
-  })
-
-  it('should be able to set the first message 2', () => {
-    cy.visit('/')
-    cy.window().then(win => {
-      console.log(win.msw)
-    })
     cy.mock('GET', 'route', 'options')
     cy.findByRole('button', { name: /first/i }).click()
     cy.findByRole('heading', { name: /first/i }).should('be.visible')
