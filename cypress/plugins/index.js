33c5d97dcd6e5958db2f5025a0ee04f15fd1af9b
Deshi Rahim <nawal.rahim@gmail.com>
2020-09-08 12:58:17 +1000

    ✨ Add ability to skip remaining tests on failure

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,10 +12,28 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+let skipRemainingTests = false
+
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  on('task', {
+    nodeLog(message) {
+      console.log(message)
+      return null
+    },
+    resetSkipRemainingTests() {
+      skipRemainingTests = false
+      return null
+    },
+    skipRemainingTests(value) {
+      if (value !== null) {
+        skipRemainingTests = value
+      }
+      return skipRemainingTests
+    },
+  })
 }
