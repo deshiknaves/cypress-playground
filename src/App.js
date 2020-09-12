926e626bfb0e951393cdaea44d9e753cb662024f
Deshi Rahim <nawal.rahim@gmail.com>
2020-09-12 10:42:41 +1000

    ✨ Add a post request to create a todo

--- src/App.js
+++ src/App.js
@@ -3,9 +3,29 @@ import './App.css'
 
 function App() {
   const [message, setMessage] = useState()
+
+  function createTodo() {
+    fetch('https://jsonplaceholder.typicode.com/todos', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: 'foo',
+        userId: 1,
+        complete: false,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then(response => response.json())
+      .then(json => {
+        console.log(json)
+        setMessage(json)
+      })
+  }
+
   return (
     <div className="App">
-      <h1>{message}</h1>
+      <h1>{JSON.stringify(message, null, 2)}</h1>
       <div>
         <button type="button" onClick={() => setMessage('First')}>
           First
@@ -15,6 +35,9 @@ function App() {
         </button>
         <button type="button" onClick={() => setMessage('Third')}>
           Third
+        </button>
+        <button type="button" onClick={createTodo}>
+          Create Todo
         </button>
       </div>
     </div>
