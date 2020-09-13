85671771bf5477df2ccb8db94f71acf7e3d36862
Deshi Rahim <nawal.rahim@gmail.com>
2020-09-14 02:16:38 +1000

    ✅ Add test where the same response is updated

--- cypress/integration/msw-requests.spec.js
+++ cypress/integration/msw-requests.spec.js
@@ -1,4 +1,4 @@
-describe('First', () => {
+describe('MSW Requests', () => {
   it("should be able to wait for a request to happen that isn't mocked before it checks for the text", () => {
     cy.visit('/')
     cy.mock('GET', 'https://jsonplaceholder.typicode.com/todos/1').as('foo')
@@ -33,7 +33,6 @@ describe('First', () => {
 
     cy.findByRole('button', { name: /first/i }).click()
     cy.waitForRequest('@foo').then(({ response }) => {
-      console.log(response.body)
       cy.getRequestCalls('@foo').then(calls => {
         expect(calls).to.have.length(1)
       })
@@ -75,5 +74,51 @@ describe('First', () => {
         'be.visible',
       )
     })
+  })
+
+  it.only('should be able to update the mock', () => {
+    cy.visit('/')
+    cy.mock(
+      'GET',
+      'https://jsonplaceholder.typicode.com/todos/1',
+      (req, res, ctx) => {
+        return res(
+          ctx.json({
+            userId: 1,
+            id: 1,
+            title: 'Lord of the rings',
+            completed: false,
+          }),
+        )
+      },
+    ).as('todos')
+
+    cy.findByRole('button', { name: /first/i }).click()
+    cy.waitForRequest('@todos')
+    cy.getRequestCalls('@todos').then(calls => {
+      expect(calls).to.have.length(1)
+    })
+    cy.findByText(/lord of the rings/i).should('be.visible')
+
+    cy.mock(
+      'GET',
+      'https://jsonplaceholder.typicode.com/todos/1',
+      (req, res, ctx) => {
+        return res(
+          ctx.json({
+            userId: 1,
+            id: 1,
+            title: 'The outsider',
+            completed: false,
+          }),
+        )
+      },
+    ).as('todos')
+    cy.findByRole('button', { name: /refetch/i }).click()
+    cy.waitForRequest('@todos')
+    cy.getRequestCalls('@todos').then(calls => {
+      expect(calls).to.have.length(2)
+    })
+    cy.findByText(/the outsider/i).should('be.visible')
   })
 })
