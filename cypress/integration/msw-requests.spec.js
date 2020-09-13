6f52455a1cd3be2f0da823de952edbddc1a03fe0
Deshi Rahim <nawal.rahim@gmail.com>
2020-09-14 02:53:48 +1000

    ✅ Remove check for first button

--- cypress/integration/msw-requests.spec.js
+++ cypress/integration/msw-requests.spec.js
@@ -1,14 +1,12 @@
 describe('MSW Requests', () => {
   it("should be able to wait for a request to happen that isn't mocked before it checks for the text", () => {
-    cy.visit('/')
     cy.mock('GET', 'https://jsonplaceholder.typicode.com/todos/1').as('foo')
+    cy.visit('/')
 
-    cy.findByRole('button', { name: /first/i }).click()
     cy.waitForRequest('@foo').then(({ response }) => {
       cy.getRequestCalls('@foo').then(calls => {
         expect(calls).to.have.length(1)
       })
-      cy.findByRole('heading', { name: /first/i }).should('be.visible')
       cy.findByText(new RegExp(response.body.title, 'i')).should('be.visible')
     })
   })
@@ -20,7 +18,7 @@ describe('MSW Requests', () => {
       'https://jsonplaceholder.typicode.com/todos/1',
       (req, res, ctx) => {
         return res(
-          ctx.delay(2000),
+          ctx.delay(1000),
           ctx.json({
             userId: 1,
             id: 1,
@@ -31,12 +29,10 @@ describe('MSW Requests', () => {
       },
     ).as('foo')
 
-    cy.findByRole('button', { name: /first/i }).click()
     cy.waitForRequest('@foo').then(({ response }) => {
       cy.getRequestCalls('@foo').then(calls => {
         expect(calls).to.have.length(1)
       })
-      cy.findByRole('heading', { name: /first/i }).should('be.visible')
       cy.findByText(new RegExp(response.body.title, 'i')).should('be.visible')
     })
   })
@@ -57,8 +53,6 @@ describe('MSW Requests', () => {
         )
       },
     )
-    cy.findByRole('button', { name: /first/i }).click()
-    cy.findByRole('heading', { name: /first/i }).should('be.visible')
     cy.findByText(/the outsider/i).should('be.visible')
   })
 
@@ -93,7 +87,6 @@ describe('MSW Requests', () => {
       },
     ).as('todos')
 
-    cy.findByRole('button', { name: /first/i }).click()
     cy.waitForRequest('@todos')
     cy.getRequestCalls('@todos').then(calls => {
       expect(calls).to.have.length(1)
